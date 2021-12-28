251e429866b5211734f495d8d99be10ffb5043ba
Kayzyu <96322205+Kayzyu@users.noreply.github.com>
2021-12-29 04:39:22 +0700

    Update

--- userbot/modules/snips.py
+++ userbot/modules/snips.py
@@ -2,6 +2,7 @@
 #
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
+
 """ Userbot module containing commands for keeping global notes. """
 
 from userbot.events import register
