cb7edac20c84c486d216d9f879b4c8167e33cd77
Kayzyu <96322205+Kayzyu@users.noreply.github.com>
2021-12-29 01:06:56 +0700

    Update dbs.py

--- userbot/modules/dbs.py
+++ userbot/modules/dbs.py
@@ -3,6 +3,7 @@
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
+
 """ Userbot module for getting information about the server. """
 
 from platform import uname
