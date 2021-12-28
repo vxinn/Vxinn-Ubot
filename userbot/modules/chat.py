83ec49245196e593a6011e1c35841025f7580cb4
Kayzyu <96322205+Kayzyu@users.noreply.github.com>
2021-12-29 00:23:34 +0700

    Update

--- userbot/modules/chat.py
+++ userbot/modules/chat.py
@@ -2,6 +2,7 @@
 #
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
+#
 """ Userbot module containing userid, chatid and log commands"""
 
 from asyncio import sleep
