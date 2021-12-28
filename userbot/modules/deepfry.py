1f8f9d6adac51bf218295de6ea6ac61a731523f6
Kayzyu <96322205+Kayzyu@users.noreply.github.com>
2021-12-29 01:17:29 +0700

    U

--- userbot/modules/deepfry.py
+++ userbot/modules/deepfry.py
@@ -2,7 +2,6 @@
 #
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
-#
 
 # Original source for the deepfrying code (used under the following
 # license): https://github.com/Ovyerus/deeppyer
