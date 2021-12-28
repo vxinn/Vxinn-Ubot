cda3b30e3fb03dcd2f85060cad8a3c21e6372e95
Kayzyu <rakaash02@gmail.com>
2021-12-28 18:31:00 +0000

    pylint: auto fixes
    Signed-off-by: Kayzyu <rakaash02@gmail.com>

--- userbot/modules/echo.py
+++ userbot/modules/echo.py
@@ -17,25 +17,25 @@ from userbot.utils import edit_delete, edit_or_reply
 from userbot.utils.events import get_user_from_event
 
 
-@register(outgoing=True, pattern=r"^.addecho(?: |$)(.*)")
+@ register(outgoing=True, pattern=r"^.addecho(?: |$)(.*)")
 async def echo(event):
     if event.reply_to_msg_id is None:
         return await event.edit("`Balas pesan Pengguna untuk menggemakan pesannya`")
-    kayzuevent = await event.edit("`Tambahkan Echo ke pengguna...`")
-    user, rank = await get_user_from_event(event, kayzuevent, nogroup=True)
+    kayzuevent=await event.edit("`Tambahkan Echo ke pengguna...`")
+    user, rank=await get_user_from_event(event, kayzuevent, nogroup=True)
     if not user:
         return
-    reply_msg = await event.get_reply_message()
-    chat_id = event.chat_id
-    user_id = reply_msg.sender_id
+    reply_msg=await event.get_reply_message()
+    chat_id=event.chat_id
+    user_id=reply_msg.sender_id
     if event.is_private:
-        chat_name = user.first_name
-        chat_type = "Personal"
+        chat_name=user.first_name
+        chat_type="Personal"
     else:
-        chat_name = event.chat.title
-        chat_type = "Group"
-    user_name = user.first_name
-    user_username = user.username
+        chat_name=event.chat.title
+        chat_type="Group"
+    user_name=user.first_name
+    user_username=user.username
     if is_echo(chat_id, user_id):
         return await event.edit("**Pengguna sudah diaktifkan dengan echo**")
     try:
@@ -52,13 +52,13 @@ async def echo(event):
         await edit_or_reply(roseevent, "Berhasil")
 
 
-@register(outgoing=True, pattern=r"^.rmecho(?: |$)(.*)")
+@ register(outgoing=True, pattern=r"^.rmecho(?: |$)(.*)")
 async def echo(event):
     if event.reply_to_msg_id is None:
         return await event.edit("Reply to a User's message to echo his messages")
-    reply_msg = await event.get_reply_message()
-    user_id = reply_msg.sender_id
-    chat_id = event.chat_id
+    reply_msg=await event.get_reply_message()
+    user_id=reply_msg.sender_id
+    chat_id=event.chat_id
     if is_echo(chat_id, user_id):
         try:
             remove_echo(chat_id, user_id)
@@ -70,11 +70,11 @@ async def echo(event):
         await event.edit("The user is not activated with echo")
 
 
-@register(outgoing=True, pattern=r"^.delecho(?: |$)(.*)")
+@ register(outgoing=True, pattern=r"^.delecho(?: |$)(.*)")
 async def echo(event):
-    input_str = event.pattern_match.group(1)
+    input_str=event.pattern_match.group(1)
     if input_str:
-        lecho = get_all_echos()
+        lecho=get_all_echos()
         if len(lecho) == 0:
             await event.edit(
                 "Anda belum mengaktifkan echo,setidaknya untuk satu pengguna dalam obrolan apa pun."
@@ -86,7 +86,7 @@ async def echo(event):
         else:
             await edit_or_reply(event, "`Echo telah di hentikan.`")
     else:
-        lecho = get_echos(event.chat_id)
+        lecho=get_echos(event.chat_id)
         if len(lecho) == 0:
             return await edit_delete(
                 event,
@@ -100,14 +100,14 @@ async def echo(event):
             await event.edit("Echo telah di hentikan.")
 
 
-@register(outgoing=True, pattern=r"^.echolist(?: |$)(.*)")
+@ register(outgoing=True, pattern=r"^.echolist(?: |$)(.*)")
 async def echo(event):  # sourcery no-metrics
-    input_str = event.pattern_match.group(1)
-    private_chats = ""
-    output_str = "**Pengguna yang mengaktifkan Echo:**\n\n"
+    input_str=event.pattern_match.group(1)
+    private_chats=""
+    output_str="**Pengguna yang mengaktifkan Echo:**\n\n"
     if input_str:
-        lsts = get_all_echos()
-        group_chats = ""
+        lsts=get_all_echos()
+        group_chats=""
         if len(lsts) > 0:
             for echos in lsts:
                 if echos.chat_type == "Personal":
@@ -130,7 +130,7 @@ async def echo(event):  # sourcery no-metrics
         if group_chats != "":
             output_str += "**Group Chats**\n" + group_chats
     else:
-        lsts = get_echos(event.chat_id)
+        lsts=get_echos(event.chat_id)
         if len(lsts) <= 0:
             return await event.edit(
                 "Tidak ada pengguna yang mengaktifkan gema dalam obrolan ini"
@@ -145,7 +145,7 @@ async def echo(event):  # sourcery no-metrics
                 private_chats += (
                     f"☞ [{echos.user_name}](tg://user?id={echos.user_id})\n"
                 )
-        output_str = (
+        output_str=(
             f"**Pengguna yang mengaktifkan Echo dalam obrolan ini adalah:**\n"
             + private_chats
         )
@@ -153,7 +153,7 @@ async def echo(event):  # sourcery no-metrics
     await edit_or_reply(event, output_str)
 
 
-@register(incoming=True, disable_edited=True)
+@ register(incoming=True, disable_edited=True)
 async def samereply(event):
     if is_echo(event.chat_id, event.sender_id) and (
         event.message.text or event.message.sticker
