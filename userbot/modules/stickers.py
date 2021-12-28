4ac1c972114d76d74354bf89ab07554217b50554
Kayzyu <96322205+Kayzyu@users.noreply.github.com>
2021-12-29 04:46:19 +0700

    Update

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -76,7 +76,7 @@ async def kang(args):
     if photo:
         splat = args.text.split()
         if not emojibypass:
-            emoji = "🔰"
+            emoji = "⚡"
         pack = 1
         if len(splat) == 3:
             pack = splat[2]  # User sent both
