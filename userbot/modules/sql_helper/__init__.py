22e6d97bbe607fbd05164e3ec62d6eaf17a1ea0f
Kayzyu <96322205+Kayzyu@users.noreply.github.com>
2021-12-30 06:58:32 +0700

    Kayzu-Ubot

--- userbot/modules/sql_helper/__init__.py
+++ userbot/modules/sql_helper/__init__.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-
 from sqlalchemy.orm import sessionmaker, scoped_session
 from userbot import DB_URI
 
