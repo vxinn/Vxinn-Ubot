e940b07791b0bd1dbba121f0e148770331699626
Kayzyu <96322205+Kayzyu@users.noreply.github.com>
2021-12-30 10:58:41 +0700

    Update core.py

--- userbot/modules/core.py
+++ userbot/modules/core.py
@@ -1,45 +1,16 @@
-# Recode By @mrismanaziz
-# FROM Geez-Project
+# Credits: @mrismanaziz
+# FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot>
+# t.me/SharingUserbot & t.me/Lunatic0de
 
-import importlib
-import logging
 import os
-import sys
 from pathlib import Path
 
-from userbot import CMD_HELP, LOGS, bot  # pylint:disable=E0602
-from userbot.events import register
-
-DELETE_TIMEOUT = 5
-
-
-def load_module(shortname):
-    if shortname.startswith("__"):
-        pass
-    elif shortname.endswith("_"):
-        path = Path(f"userbot/modules/{shortname}.py")
-        name = "userbot.modules.{}".format(shortname)
-        spec = importlib.util.spec_from_file_location(name, path)
-        mod = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(mod)
-        LOGS.info("Successfully imported " + shortname)
-    else:
-
-        path = Path(f"userbot/modules/{shortname}.py")
-        name = "userbot.modules.{}".format(shortname)
-        spec = importlib.util.spec_from_file_location(name, path)
-        mod = importlib.util.module_from_spec(spec)
-        mod.bot = bot
-        mod.LOGS = LOGS
-        mod.CMD_HELP = CMD_HELP
-        mod.logger = logging.getLogger(shortname)
-        spec.loader.exec_module(mod)
-        # for imports
-        sys.modules["userbot.modules." + shortname] = mod
-        LOGS.info("Successfully imported " + shortname)
+from userbot import CMD_HELP, bot
+from userbot.events import man_cmd
+from userbot.utils import edit_or_reply, load_module, remove_plugin, reply_id
 
 
-@register(outgoing=True, pattern=r"^\.install")
+@bot.on(man_cmd(outgoing=True, pattern="install$"))
 async def _(event):
     if event.fwd_from:
         return
@@ -59,20 +30,59 @@ async def _(event):
                 await event.edit(
                     "**Plugin** `{}` **Berhasil di install**".format(
                         os.path.basename(downloaded_file_name)
+                    )
                 )
-            )
             else:
                 os.remove(downloaded_file_name)
                 await event.edit("**Error!** Plugin ini sudah terinstall di userbot.")
-        except Exception as e:  # pylint:disable=C0103,W0703
+        except Exception as e:
             await event.edit(str(e))
             os.remove(downloaded_file_name)
+
+
+@bot.on(man_cmd(outgoing=True, pattern=r"psend ([\s\S]*)"))
+async def send(event):
+    reply_to_id = await reply_id(event)
+    input_str = event.pattern_match.group(1)
+    the_plugin_file = f"./userbot/modules/{input_str}.py"
+    if os.path.exists(the_plugin_file):
+        caat = await event.client.send_file(
+            event.chat_id,
+            the_plugin_file,
+            force_document=True,
+            thumb="userbot/resources/logo.jpg",
+            allow_cache=False,
+            reply_to=reply_to_id,
+            caption=f"➠ **Nama Plugin:** `{input_str}`",
+        )
+        await event.delete()
+    else:
+        await edit_or_reply(event, "**ERROR: Modules Tidak ditemukan**")
+
+
+@bot.on(man_cmd(outgoing=True, pattern=r"uninstall (?P<shortname>\w+)"))
+async def uninstall(event):
+    if event.fwd_from:
+        return
+    shortname = event.pattern_match["shortname"]
+    dir_path = f"./userbot/modules/{shortname}.py"
+    try:
+        remove_plugin(shortname)
+        os.remove(dir_path)
+        await event.edit(f"**Berhasil Menghapus Modules** `{shortname}`")
+    except OSError as e:
+        await event.edit("**ERROR:** `%s` : %s" % (dir_path, e.strerror))
 
 
 CMD_HELP.update(
     {
         "core": "**Plugin : **`core`\
-        \n\n  •  **Syntax :** `.install` <reply ke file plugins>\
-        \n  •  **Function : **Untuk Menginstall plugins ubot secara instan.\
- }
+        \n\n  •  **Syntax :** `.install` <reply ke file module>\
+        \n  •  **Function : **Untuk Menginstall module userbot secara instan.\
+        \n\n  •  **Syntax :** `.uninstall` <nama module>\
+        \n  •  **Function : **Untuk Menguninstall / Menghapus module userbot secara instan.\
+        \n\n  •  **Syntax :** `.psend` <nama module>\
+        \n  •  **Function : **Untuk Mengirim module userbot secara instan.\
+    "
+    }
 )
