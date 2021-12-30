14bbff57017e27efbd86c53f44d8f2c5f98f981f
Kayzyu <rakaash02@gmail.com>
2021-12-30 01:35:34 +0000

    pylint: auto fixes
    Signed-off-by: Kayzyu <rakaash02@gmail.com>

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -12,9 +12,9 @@ from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
 from userbot import BOT_VER, LOGS, bot
 from userbot.modules import ALL_MODULES
 
-INVALID_PH =("\nERROR: The Phone No. entered is INVALID"
-    "\n Tip: Use Country Code along with number."
-    "\n or check your phone number and try again !")
+INVALID_PH = ("\nERROR: The Phone No. entered is INVALID"
+              "\n Tip: Use Country Code along with number."
+              "\n or check your phone number and try again !")
 
 try:
     bot.start()
@@ -30,4 +30,5 @@ LOGS.info(f"⚡Kayzu - Ubot⚡ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
-else:bot.run_until_disconnected()
+else:
+    bot.run_until_disconnected()
