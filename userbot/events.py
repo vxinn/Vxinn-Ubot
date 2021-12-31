681c6edfce85f340441666600adc112ecbaa4c31
Kayzyu <96322205+Kayzyu@users.noreply.github.com>
2021-12-31 09:13:38 +0700

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -194,7 +194,7 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**✘ MAN-USERBOT ERROR REPORT ✘**\n\n"
+                    text = "**✘ KAYZU-UBOT ERROR REPORT ✘**\n\n"
                     link = "[Group Support](https://t.me/SharingUserbot)"
                     text += "Jika mau, Anda bisa melaporkan error ini, "
                     text += f"Cukup forward saja pesan ini ke {link}.\n\n"
