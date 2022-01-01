2d0703d5ebd5c5c19548fb1065dba9bef1e6d6bf
Kayzyu <96322205+Kayzyu@users.noreply.github.com>
2022-01-01 20:07:41 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -5,8 +5,7 @@
 #
 """ Userbot start point """
 
-from importlib
-from import import_module
+from importlib from import import_module
 from sys import argv
 
 from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
@@ -29,7 +28,7 @@ for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
 
-LOGS.info(f"✨Kayzu-Ubot✨ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+LOGS.info(f"⚡Kayzu-Ubot⚡ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
 
 
 if len(argv) not in (1, 3, 4):
