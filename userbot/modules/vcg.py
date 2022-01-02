453f996617d215da79cb6104d85d7383a2d7beaf
SkyzuProject <92871568+Skyzu@users.noreply.github.com>
2022-01-02 14:18:32 +0700

    SKYZU GANTENG

--- userbot/modules/vcg.py
+++ userbot/modules/vcg.py
@@ -1,103 +1,84 @@
 # Thanks Full To Team Ultroid
+# Ported By @skyzu
 
 from telethon.tl.functions.channels import GetFullChannelRequest as getchat
+from telethon.tl.functions.phone import CreateGroupCallRequest as startvc
+from telethon.tl.functions.phone import DiscardGroupCallRequest as stopvc
 from telethon.tl.functions.phone import GetGroupCallRequest as getvc
 from telethon.tl.functions.phone import InviteToGroupCallRequest as invitetovc
 
-from telethon.tl.types import ChatAdminRights
 from userbot import CMD_HELP
 from userbot.events import register
 
-NO_ADMIN = "`Maaf Kamu Bukan Admin 👮"
+NO_ADMIN = "`Maaf Kamu Bukan Admin!"
 
 
 async def get_call(event):
-    Kayzu = await event.client(getchat(event.chat_id))
-    Kayzu = await event.client(getvc(Kayzu.full_chat.call))
-    return Kayzu.call
+    geez = await event.client(getchat(event.chat_id))
+    vcky = await event.client(getvc(geez.full_chat.call))
+    return vcky.call
 
 
 def user_list(l, n):
     for i in range(0, len(l), n):
-        yield l[i: i + n]
+        yield l[i : i + n]
 
 
-@register(outgoing=True, pattern=r"^\.startvcg", groups_only=True)
-async def _(e):
-    chat = await e.get_chat()
+@register(outgoing=True, pattern=r"^\.startvc$", groups_only=True)
+async def start_voice(td):
+    chat = await td.get_chat()
     admin = chat.admin_rights
     creator = chat.creator
 
     if not admin and not creator:
-        return await e.edit(NO_ADMIN)
-    new_rights = ChatAdminRights(invite_users=True)
+        return await td.edit(NO_ADMIN)
     try:
-        await e.client(startvcg(e.chat_id))
-        await e.edit("`Memulai Obrolan Suara`")
+        await td.client(startvc(td.chat_id))
+        await td.edit("`Voice Chat Started...`")
     except Exception as ex:
-        await e.edit(f"`{str(ex)}`")
+        await td.edit(f"`{str(ex)}`")
 
 
-@register(outgoing=True, groups_only=True, pattern=r"^\.stopvcg")
-async def stop_voice(c):
-    chat = await c.get_chat()
+@register(outgoing=True, pattern=r"^\.stopvc$", groups_only=True)
+async def stop_voice(td):
+    chat = await td.get_chat()
     admin = chat.admin_rights
     creator = chat.creator
 
     if not admin and not creator:
-        return await e.edit(NO_ADMIN)
-    new_rights = ChatAdminRights(invite_users=True)
+        return await td.edit(NO_ADMIN)
     try:
-        await c.client(stopvcg(await get_call(c)))
-        await c.edit("`Mematikan Obrolan Suara`")
+        await td.client(stopvc(await get_call(td)))
+        await td.edit("`Voice Chat Stopped...`")
     except Exception as ex:
-        await c.edit(f"**ERROR:** `{ex}`")
+        await td.edit(f"`{str(ex)}`")
 
 
-@register(outgoing=True, pattern=r"^\.vcginvite", groups_only=True)
-async def _(e):
-    await e.edit("`Sedang Mengivinte Jamet Tele...`")
+@register(outgoing=True, pattern=r"^\.vcinvite", groups_only=True)
+async def vc_invite(td):
+    await td.edit("`Sedang Mengivinte Member...`")
     users = []
     z = 0
-    async for x in e.client.iter_participants(e.chat_id):
+    async for x in td.client.iter_participants(td.chat_id):
         if not x.bot:
             users.append(x.id)
     hmm = list(user_list(users, 6))
     for p in hmm:
         try:
-            await e.client(invitetovc(call=await get_call(e), users=p))
+            await td.client(invitetovc(call=await get_call(td), users=p))
             z += 6
         except BaseException:
             pass
-    await e.edit(f"`Invited {z} users`")
-
-
-@register(outgoing=True, pattern=r"^\.vctitle", groups_only=True)
-async def _(Kayzu):
-    title = e.pattern_match.group(1)
-    chat = await diorbot.get_chat()
-    admin = chat.admin_rights
-    creator = chat.creator
-
-    if not title:
-        return await Kayzu.edit("**Silahkan Masukan Title Obrolan Suara Grup**")
-
-    if not admin and not creator:
-        return await Kayzu.edit(f"**Maaf {ALIVE_NAME} Bukan Admin 👮**")
-    try:
-        await diorbot.edit(settitle(call=await get_call(e), title=title.strip()))
-        await diorbot.edit(f"`Berhasil Mengubah Judul VCG Menjadi` `{title}`")
-    except Exception as ex:
-        await diorbot.edit(f"**ERROR:** `{ex}`")
+    await td.edit(f"`Invited {z} users`")
 
 
 CMD_HELP.update(
     {
-        "vcg": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.startvcg`\
+        "vcg": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.startvc`\
          \n↳ : Start Group Call in a group.\
-         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.stopvcg`\
+         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.stopvc`\
          \n↳ : `Stop Group Call in a group.`\
-         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.vcginvite`\
+         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.vcinvite`\
          \n↳ : Invite all members of group in Group Call. (You must be joined)."
     }
 )
