5f33c3a40ff4805236d09163a1b025aa345e9acd
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-01-07 01:40:06 +0700

    Update vcjoin.py

--- userbot/modules/vcjoin.py
+++ userbot/modules/vcjoin.py
@@ -47,12 +47,10 @@ async def leaver(event):
         del VIDEO_ON[chat]
     await event.eor(get_string("vcbot_1"))
 
-
-CMD_HELP.update(
-    {
-        "vcjoin": n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.joinvc`
-        \n↳: 'Join Voice Chat.'
-        \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.leavevc`
-        \n↳: `Leave The Voice Chat.`
-    }
-)
+CMD_HELP.update({
+   "vcjoin": 
+n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.joinvc`
+\n↳: 'Join Voice Chat.'
+\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.leavevc`
+\n↳: `Leave The Voice Chat.`
+})
