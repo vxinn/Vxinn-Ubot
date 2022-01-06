c3711088501169aa316514fef7a4f5f66681e959
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-01-07 01:48:13 +0700

    Update kontol

--- userbot/modules/vcg.py
+++ userbot/modules/vcg.py
@@ -8,6 +8,7 @@ from telethon.tl.functions.phone import DiscardGroupCallRequest as stopvc
 from telethon.tl.functions.phone import GetGroupCallRequest as getvc
 from telethon.tl.functions.phone import InviteToGroupCallRequest as invitetovc
 
+from pytgcalls.exceptions import NotConnectedError
 
 from userbot import CMD_HELP
 from userbot.events import register
@@ -76,6 +77,40 @@ async def _(kyy):
     await kyy.edit(f"`Menginvite {z} Member`")
 
 
+@vc_asst("joinvc")
+async def join_(event):
+    if len(event.text.split()) > 1:
+        chat = event.text.split()[1]
+        try:
+            chat = await event.client.parse_id(chat)
+        except Exception as e:
+            return await event.eor(get_string("vcbot_2").format(str(e)))
+    else:
+        chat = event.chat_id
+    ultSongs = Player(chat, event)
+    if not kayzu.group_call.is_connected:
+        await kayzu.vc_joiner()
+
+
+@vc_asst("(leavevc|stopvc)")
+async def leaver(event):
+    if len(event.text.split()) > 1:
+        chat = event.text.split()[1]
+        try:
+            chat = await event.client.parse_id(chat)
+        except Exception as e:
+            return await event.eor(get_string("vcbot_2").format(str(e)))
+    else:
+        chat = event.chat_id
+    Kayzu = Player(chat)
+    await kayzu.group_call.stop()
+    if CLIENTS.get(chat):
+        del CLIENTS[chat]
+    if VIDEO_ON.get(chat):
+        del VIDEO_ON[chat]
+    await event.eor(get_string("vcbot_1"))
+
+
 CMD_HELP.update(
     {
         "vcg": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.startvc`\
@@ -84,5 +119,9 @@ CMD_HELP.update(
          \n↳ : `Menghentikan Obrolan Suara Pada Group.`\
          \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.vcinvite`\
          \n↳ : Invite semua member yang berada di group."
+         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.joinvc`
+         \n↳: 'Join Voice Chat.'
+         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.leavevc`
+         \n↳: `Leave The Voice Chat.`
     }
 )
