6e542c18abbd5d42b191514347467ceb343354e6
vxinn <96599804+vxinn@users.noreply.github.com>
2022-01-10 15:13:22 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -382,8 +382,9 @@ with bot:
         quit(1)
 
 
+
 async def check_alive():
-    await bot.send_file(BOTLOG_CHATID, ALIVE_LOGO, caption=f"**ᴠxɪɴ-ᴜʙᴏᴛ Berhasil Diaktifkan☯**\n━━━━━━━━━━━━━━━━━━━\n✧ **ʙᴏᴛ ᴏꜰ :** ᴠxɪɴ\n✧ **ʙᴏᴛ ᴠᴇʀ :** 7.0\n━━━━━━━━━━━━━━━━━━━\n✧ **ꜱᴜᴘᴘᴏʀᴛ​ :** @KayzuSupport\n✧ **ᴄʜᴀɴɴᴇʟ​ :** @aboutxin \n━━━━━━━━━━━━━━━━━━━")
+    await bot.send_file(BOTLOG_CHATID, ALIVE_LOGO, caption=f"**ᴋᴀʏᴢᴜ-ᴜʙᴏᴛ Berhasil Diaktifkan🌸**\n━━━━━━━━━━━━━━━━━━━\n✧ **ʙᴏᴛ ᴏꜰ :** {ALIVE_NAME}\n✧ **ʙᴏᴛ ᴠᴇʀ :** 7.0\n━━━━━━━━━━━━━━━━━━━\n✧ **ꜱᴜᴘᴘᴏʀᴛ​ :** @KayzuSupport\n✧ **ᴄʜᴀɴɴᴇʟ​ :** @kayzuchannel \n━━━━━━━━━━━━━━━━━━━")
     return
 
 
