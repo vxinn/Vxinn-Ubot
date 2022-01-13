3b3bbdacd1cdb73fe83261098717cb9799bc285e
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-01-13 15:23:59 +0700

    Update __help.py

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -13,10 +13,16 @@ async def yardim(event):
     try:
         tgbotusername = BOT_USERNAME
         if tgbotusername is not None:
-            results = await event.client.inline_query(tgbotusername, "@KayzuSupport")
-            await results[0].click(event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True)
+            results = await event.client.inline_query(tgbotusername, "@IdNyaKosong")
+            await results[0].click(
+                event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
+            )
             await event.delete()
         else:
-            await event.edit("`Botnya tidak berfungsi! Silahkan atur Bot Token dan Username dengan benar. Modul telah dihentikan.`")
+            await event.edit(
+                "`Botnya tidak berfungsi! Silahkan atur Bot Token dan Username dengan benar. Modul telah dihentikan.`"
+            )
     except Exception:
-        return await event.edit("`Anda tidak dapat mengirim hasil sebaris dalam hal ini ke chat (disebabkan oleh Mengirim Inline Sebaris)`")
+        return await event.edit(
+            "`Anda tidak dapat mengirim hasil sebaris dalam hal ini ke chat (disebabkan oleh Mengirim Inline Sebaris)\ngunakan perintah .inlineon`"
+        )
