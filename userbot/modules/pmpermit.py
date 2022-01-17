9af3bf7bcc18907ebcb7997937010a74c7649d4f
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-01-17 13:35:36 +0700

    Update pmpermit.py

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -3,7 +3,6 @@
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
 
-"""Userbot module for keeping control who PM you."""
 
 from sqlalchemy.exc import IntegrityError
 from telethon.tl.functions.contacts import BlockRequest, UnblockRequest
