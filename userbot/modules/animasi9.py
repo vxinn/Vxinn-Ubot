4a093cfc315a84ddf3af53ef9a93e554b75d66aa
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-01-19 06:57:19 +0700

    Create animasi9.py

---
+++ userbot/modules/animasi9.py
@@ -0,0 +1,61 @@
+from time import sleep
+
+from userbot import CMD_HELP
+from userbot.events import register
+
+
+@register(outgoing=True, pattern="^.sange$")
+async def koc(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        await e.edit("SAYANGGGGGGGGG 💕")
+        sleep(1)
+        await e.edit("💝💘💓💗")
+        sleep(1)
+        await e.edit("💞💕💗💘")
+        sleep(1)
+        await e.edit("💝💘💓💗")
+        sleep(1)
+        await e.edit("💞💕💗💘")
+        sleep(1)
+        await e.edit("💘💞💗💕")
+        sleep(1)
+        await e.edit("💘💞💕💗")
+        sleep(1)
+        await e.edit("EMMMMMM🥺🥺🥺")
+        sleep(1)
+        await e.edit("💝💘💓💗")
+        await e.edit("💞💕💗💘")
+        await e.edit("💘💞💕💗")
+        await e.edit("SAYANG")
+        sleep(1)
+        await e.edit("AKU 👉👈")
+        sleep(1)
+        await e.edit("SANGE 👉👈 😘😘")
+        sleep(1)
+        await e.edit("💘💘💘💘")
+        sleep(1)
+        await e.edit("SAYANG")
+        sleep(1)
+        await e.edit("AYO NGEWE🤭🤭")
+        sleep(1)
+        await e.edit("PLISS🥺🥺")
+        sleep(1)
+        await e.edit("AKU SANGE😋😋")
+        sleep(1)
+        await e.edit("I LOVE YOUUUU")
+        sleep(1)
+        await e.edit("AH AH AH BEIBB")
+        sleep(1)
+        await e.edit("💦💦💦💦")
+        sleep(1)
+        await e.edit("OH BABY")
+        sleep(1)
+        await e.edit("AKU SAYANG KAMU💞")
+
+
+CMD_HELP.update({
+    "animasi9": "`.cinta`\
+    \nUsage: mengirim cintamu ke seseorang.\
+    \n\n`.sange` :\
+    \nUsage: cobalah sndiri ngentot."
+})
