86d38d40f56572ef4e19c735b439890f41a45987
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-01-19 07:16:23 +0700

    Update asupan.py

--- userbot/modules/asupan.py
+++ userbot/modules/asupan.py
@@ -10,19 +10,27 @@ from userbot.events import register
 @register(outgoing=True, pattern=r"^\.asupan$")
 async def _(event):
     try:
-        response = requests.get(
-            "https://api-tede.herokuapp.com/api/asupan/ptl").json()
+        response = requests.get("https://api-tede.herokuapp.com/api/asupan/ptl").json()
         await event.client.send_file(event.chat_id, response["url"])
         await event.delete()
     except Exception:
         await event.edit("**Tidak bisa menemukan video asupan.**")
 
 
+@register(outgoing=True, pattern=r"^\.wibu$")
+async def _(event):
+    try:
+        response = requests.get("https://api-tede.herokuapp.com/api/asupan/wibu").json()
+        await event.client.send_file(event.chat_id, response["url"])
+        await event.delete()
+    except Exception:
+        await event.edit("**Tidak bisa menemukan video wibu.**")
+
+
 @register(outgoing=True, pattern=r"^\.chika$")
 async def _(event):
     try:
-        response = requests.get(
-            "https://api-tede.herokuapp.com/api/chika").json()
+        response = requests.get("https://api-tede.herokuapp.com/api/chika").json()
         await event.client.send_file(event.chat_id, response["url"])
         await event.delete()
     except Exception:
@@ -35,10 +43,10 @@ async def _(event):
         response = requests.get(
             "https://api-alphabot.herokuapp.com/api/asupan/bocil?apikey=Alphabot"
         ).json()
-        await event.client.send_file(event.chat_id, response["url"])
+        await event.client.send_file(event.chat_id, response["result"])
         await event.delete()
     except Exception:
-        await event.edit("**Tidak bisa menemukan video bocil.**")
+        await event.edit("**Tidak bisa menemukan video asupan bocil.**")
 
 
 CMD_HELP.update(
@@ -46,6 +54,8 @@ CMD_HELP.update(
         "asupan": "**Plugin : **`asupan`\
         \n\n  •  **Syntax :** `.asupan`\
         \n  •  **Function : **Untuk Mengirim video asupan secara random.\
+        \n\n  •  **Syntax :** `.wibu`\
+        \n  •  **Function : **Untuk Mengirim video wibu secara random.\
         \n\n  •  **Syntax :** `.chika`\
         \n  •  **Function : **Untuk Mengirim video chikakiku secara random.\
         \n\n  •  **Syntax :** `.bocil`\
