6bb00d30d0f52405bb994161d604a489932e9e8f
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-01-19 07:23:49 +0700

    Update animasi9.py

--- userbot/modules/animasi9.py
+++ userbot/modules/animasi9.py
@@ -53,7 +53,5 @@ async def koc(e):
         await e.edit("AKU SAYANG KAMU💞")
 
 
-CMD_HELP.update({
-      "animasi9": "`.sange`\
-      \nUsage: Sange Ke Orang.\
-})
+CMD_HELP.update({"animasi9": "`.sange`\
+                 \nUsage: Sange Ke Orang.\})
